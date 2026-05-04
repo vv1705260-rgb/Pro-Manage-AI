de670b5c2b1f67424c941994ccbcfada8e5e5a31
Vaishali <vv1705260@gmail.com>
2026-05-04 18:50:39 +0530

    Update taskRoutes.js

--- server/routers/taskRoutes.js
+++ server/routers/taskRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const auth = require('../middleware/authMiddleware');
 const { getTasks, createTask } = require('../controllers/taskController');
-
+router.put('/:id', auth, updateTask);
 router.get('/', auth, getTasks);
 router.post('/', auth, createTask);
 
