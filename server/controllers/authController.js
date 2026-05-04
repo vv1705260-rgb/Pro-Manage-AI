a5c82ac618a837b2bebd647a5bd67a12790330ea
Vaishali <vv1705260@gmail.com>
2026-05-04 18:22:42 +0530

    Implement user registration and login functions

---
+++ server/controllers/authController.js
@@ -0,0 +1,26 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await User.create({ email, password: hashed });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: 'User not found' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: 'Wrong password' });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ token });
+};
